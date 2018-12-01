dc4bfafc01b594c5c3a9b9079fb14fe9ce7ac41a
EHSchmitt <eric@Earth.localdomain>
2018-12-01 17:57:48 -0500

    changed lists in vpc

--- Modules/dmz/vpc.tf
+++ Modules/dmz/vpc.tf
@@ -110,7 +110,7 @@ resource "aws_security_group" "publicsg" {
     from_port = 80
     to_port = 80
     protocol = "tcp"
-    cidr_blocks = "${var.vpc_access}"
+    cidr_blocks = ["${var.vpc_access}"]
 
   }
 
@@ -118,13 +118,13 @@ resource "aws_security_group" "publicsg" {
     from_port = 443
     to_port = 443
     protocol = "tcp"
-    cidr_blocks = "${var.vpc_access}"
+    cidr_blocks = ["${var.vpc_access}"]
   }
   ingress {
     from_port = 22
     to_port = 22
     protocol = "tcp"
-    cidr_blocks = "${var.vpc_access}"
+    cidr_blocks = ["${var.vpc_access}"]
   }
 
   vpc_id="${aws_vpc.dmz.id}"
