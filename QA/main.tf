b2174d5a6d52845d428687b9fbe217f317b9d862
galloh@mail.sacredheart.edu <galloh>
2019-02-06 20:03:43 -0500

    hgallo calling iam module

--- QA/main.tf
+++ QA/main.tf
@@ -1,17 +1,17 @@
-provider "aws"{
+provider "aws" {
   region = "us-east-1"
 }
 
-# vpc 
+# vpc
 
 module "vpc" {
-
-  source = "../Modules/dmz" 
-  vpc_cidr = "10.0.0.0/16" 
+  source       = "../Modules/dmz"
+  vpc_cidr     = "10.0.0.0/16"
   vpc_frontend = "10.0.1.0/24"
   vpc_applayer = "10.0.2.0/24"
-  vpc_database = "10.0.3.0/24"  
+  vpc_database = "10.0.3.0/24"
 }
 
-
-
+module "policies" {
+  source = "../IAM"
+}
