65e102db6bf748a4e67106fe9275d2b01c6cfea7
root <root@Earth.localdomain>
2019-04-09 11:52:41 -0400

    modified sg of bastion added public ip

--- QA/main.tf
+++ QA/main.tf
@@ -2,9 +2,8 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# vpc
 
-module "vpc" {
+ module "vpc" {
   source       = "../Modules/dmz"
   vpc_cidr     = "10.0.0.0/16"
   vpc_frontend = "10.0.1.0/24"
@@ -12,7 +11,7 @@ module "vpc" {
   vpc_database = "10.0.3.0/24"
 }
 
-module "policies" {
-  source              = "../Modules/IAM"
-  account_number_root = ""
-}
+#module "policies" {
+#  source              = "../Modules/IAM"
+#  account_number_root = "592662970146"
+#}
